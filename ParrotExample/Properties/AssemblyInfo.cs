8c8eeb0bbcf530b1d5629894da984d19a1f21ca6
Sebastian Lierka <slierka@dotnetservices.de>
2018-11-08 14:36:43 +0100

    info

--- ParrotExample/Properties/AssemblyInfo.cs
+++ ParrotExample/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("PapageiTest")]
+[assembly: AssemblyTitle("ParrotExample")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PapageiTest")]
+[assembly: AssemblyCompany("dotnet Services")]
+[assembly: AssemblyProduct("ParrotExample")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
